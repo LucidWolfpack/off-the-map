85c2e4a2d2f62bc4b14879f876dd5068f5419244
TheApeMan <acrussell1995@gmail.com>
2022-11-13 17:03:36 -0500

    update controllers

--- controllers/urban.js
+++ controllers/urban.js
@@ -8,9 +8,48 @@ async function index(req, res) {
   })
 }
 
+async function show(req, res){
+  const urban = await Urban.findById(req.params.id)
+  res.render("urban/show", {
+    urban:urban
+  })
+}
 
+async function create(req, res){
+  req.body.complete = !!req.body.complete
+  const urban = await Urban.create(req.body)
+  res.redirect('/urban')
+}
+
+async function newUrban(req, res){
+  res.render("urban/new")
+}
+
+async function deleteUrban(req, res){
+  await Urban.findByIdAndDelete(req.params.id)
+  res.redirect('/urban')
+}
+
+async function editUrban(req, res){
+  const urban = await Urban.findById(req.params.id)
+  res.render("urban/edit", {
+    urban:urban
+  })
+}
+
+async function updateUrban(req, res){
+  req.body.complete = !!req.body.complete
+  const urban = await Urban.findByIdAndUpdate(req.params.id, req.body, {new:true})
+  res.redirect(`/urban${urban._id}`)
+}
 
 
 export{
   index,
+  show,
+  create,
+  newUrban as new,
+  deleteUrban as delete,
+  editUrban as edit,
+  updateUrban as update
 }
--- routes/urban.js
+++ routes/urban.js
@@ -4,7 +4,12 @@ import * as urbanController from "../controllers/urban.js"
 const router = Router()
 
 router.get('/', urbanController.index)
-
+router.get('/new', urbanController.new)
+router.get('/:id', urbanController.show)
+router.post('/', urbanController.create)
+router.delete('/:id', urbanController.delete)
+router.get('/:id/edit', urbanController.edit)
+router.put('/:id', urbanController.update)
 
 
 export{
